3bb9258ea2dd6b97ec5746df027390bf714b236b
Divesh Kannan T <diveshkannan.tr@gmail.com>
2026-08-26 16:39:56 +0530

    Add height input and pyramid printing logic
    Implement input validation for pyramid height and print pyramid structure.

---
+++ Pset6/Mario-more/Replication method/mario.py
@@ -0,0 +1,18 @@
+while True:
+    try:
+           height = int(input("Height: "))
+
+    except ValueError:
+        print("Only integers.")
+
+    else:
+        if  1<height<8 :
+            print("Height: ",height)
+            break
+for i in range(height):
+     print(" "*(height-i-1),end="")
+     print("#"*(i+1),end="")
+     print("  ",end="")
+     print("#"*(i+1))
+
+
